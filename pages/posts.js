f163fbbc0a671d0d0e7a28dd4a6d3b62b3bca776
Alex <Alex.mclean@bennetts.co.uk>
2023-02-06 15:12:17 +0000

    changes url to live url for fetch request

--- pages/posts.js
+++ pages/posts.js
@@ -1,25 +1,26 @@
 import { useEffect, useState } from "react";
 
 export default function Posts() {
-    const [posts, setPosts] = useState(null);
+  const [posts, setPosts] = useState(null);
 
-    useEffect(() => {
-        async function fetchPosts() {
-            const res = await fetch("http://localhost:3000/api/posts");
-            const postsList = await res.json();
-            setPosts(postsList);
-        }
-        fetchPosts()
-    });
+  useEffect(() => {
+    async function fetchPosts() {
+      const res = await fetch(
+        "https://impersonisation-agent.vercel.app/api/posts"
+      );
+      const postsList = await res.json();
+      setPosts(postsList);
+    }
+    fetchPosts();
+  });
 
-    return (
-        <>
-        <ul>
-            {posts?.map(post => {
-                return (<li key={post.id}>{post.text}</li>)
-            })}
-        </ul>
-
-        </>
-    )
+  return (
+    <>
+      <ul>
+        {posts?.map((post) => {
+          return <li key={post.id}>{post.text}</li>;
+        })}
+      </ul>
+    </>
+  );
 }
